ad16e2859aba09ade824a146949897da02fc9f8c
deniz.dizman <deniz.dizman@085a22d6-ddbd-42e6-1810-c1e1748c5ad8>
2010-05-08 07:44:30 +0000

    completed live graph view

--- trunk/botpit/src/core/Graph.java
+++ trunk/botpit/src/core/Graph.java
@@ -1,5 +1,8 @@
 package core;
-
+/**
+ * Most of the code here is a modified version of the code from 
+ * opentestbed. http://code.google.com/p/opentestbed
+ */
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
@@ -8,7 +11,6 @@ import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.swing.JFrame;
@@ -29,7 +31,7 @@ public class Graph {
 	private Timer timer;
 	private Botpit gi;
 	private ChartPanel chartPanel = new ChartPanel(null);
-	private Map<Integer, List<Map<String, Double>>> stats = new HashMap<Integer, List<Map<String, Double>>>();
+//	private Map<Integer, List<Map<String, Double>>> stats = new HashMap<Integer, List<Map<String, Double>>>();
 	StringBuffer sb = new StringBuffer();
 	public Graph(Botpit gi) {
 		this.gi = gi;
@@ -39,19 +41,20 @@ public class Graph {
 		update_graph();
 		frame.setVisible(true);
 	}
+
 	private void create_frame() {
 		frame = new JFrame("progress");
 		frame.setSize(1000,600);
-		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		progress_bar = new JProgressBar(0,gi.NUMSIMROUNDS);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		progress_bar = new JProgressBar(0,Botpit.NUMSIMROUNDS);
 		progress_bar.setStringPainted(true);
 		frame.add(progress_bar, BorderLayout.NORTH);
-		frame.add(chartPanel,BorderLayout.SOUTH);
+		frame.add(chartPanel,BorderLayout.CENTER);
 	}
 	private JFreeChart calculate_chart(int snapshot) {
 		Map<String, Double> playerToBankRoll = new HashMap<String, Double>();
 		Map<String, XYSeries> playerToXYSeries = new HashMap<String, XYSeries>();
-		for (int i=0;i<gi.NUMPLAYERS;i++) {
+		for (int i=0;i<Botpit.NUMPLAYERS;i++) {
 			playerToBankRoll.put("seat " + i, new Double(0));
 			playerToXYSeries.put("seat " + i, new XYSeries("seat "+i));
 		}
@@ -69,10 +72,10 @@ public class Graph {
 	private JFreeChart createJFreeChart(
 			Map<String, Double> playerToBankRoll,
 			XYSeriesCollection xySeriesCollection, int snapshot) {
-		final JFreeChart chart = ChartFactory.createXYLineChart("Stack", "Games", "Bankroll",
+		final JFreeChart chart = ChartFactory.createXYLineChart("Live Stack Graph", "Games", "Stack",
 				xySeriesCollection, PlotOrientation.VERTICAL, true, false, false);
 		
-		for (int i=0;i<Botpit.NUMSIMROUNDS;i++) {
+		for (int i=0;i<Botpit.NUMPLAYERS;i++) {
 			String playerName = "seat "+i;
 			double finalBankroll = playerToBankRoll.get(playerName);
 			DecimalFormat moneyFormat = new DecimalFormat("0.00");
@@ -94,9 +97,10 @@ public class Graph {
 	private void calculateBankrolls(
 			Map<String, Double> playerToBankRoll,
 			Map<String, XYSeries> playerToXYSeries, int snapshot) {
-		for (int game = 0; game < Math.min(gi.NUMSIMROUNDS, snapshot); game++) {
-			for (int i=0;i<gi.NUMPLAYERS;i++) {
-				double playerBankRoll = playerToBankRoll.get("seat "+i);
+		for (int game = 0; game < Math.min(Botpit.NUMSIMROUNDS, snapshot); game++) {
+			for (int i=0;i<Botpit.NUMPLAYERS;i++) {
+				double playerBankRoll = gi.stacks[i];
+				playerToBankRoll.put("seat "+i, playerBankRoll);
 				playerToXYSeries.get("seat "+i).add(game+1, playerBankRoll);
 			}
 		}
@@ -107,18 +111,19 @@ public class Graph {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				int snapshot = gi.current_hand;
+				if (snapshot == Botpit.NUMSIMROUNDS) timer.stop();
 				long now = System.currentTimeMillis();
 				long past_time = now - Botpit.game_start_time;
-				long gamespeed_avg = snapshot > 0 ? (past_time / snapshot) : 0;
-				Date expectedEnd = new Date(System.currentTimeMillis() + (gi.NUMSIMROUNDS - snapshot) * gamespeed_avg);
+				float gamespeed_avg = snapshot > 0 ? (snapshot / (past_time/1000)) : 0;
+				Date expectedEnd = new Date(System.currentTimeMillis() + (Botpit.NUMSIMROUNDS - snapshot) * (int)gamespeed_avg);
 				long progress = snapshot * 100 / Botpit.NUMSIMROUNDS;
 				progress_bar.setValue(snapshot);
 				SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yy HH:mm");
 				sb.append("Completed:");
 				sb.append(snapshot); sb.append("/");
-				sb.append(gi.NUMSIMROUNDS);
+				sb.append(Botpit.NUMSIMROUNDS);
 				sb.append(" - %");sb.append(progress);
-				sb.append(" Speed:");sb.append((Math.round(gamespeed_avg* 100)) / 100D);
+				sb.append(" Speed (g/s):");sb.append((Math.round(gamespeed_avg* 100)) / 100D);
 				sb.append(" ETF:");sb.append(dateFormat.format(expectedEnd));
 				progress_bar.setString(sb.toString());
 				sb.setLength(0);
